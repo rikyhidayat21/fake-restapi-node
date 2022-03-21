698c386fdc0b4f8757daf89c173144a0528d5ae2
rikyhidayat21 <rikyhidayat21@gmail.com>
2022-03-21 22:25:42 +0700

    update flow

---
+++ README.md
@@ -0,0 +1,14 @@
+# FAKE REST API NODE
+
+## HOW TO INSTALL
+```sh
+npm i fake-restapi-node
+```
+
+## USAGE
+
+```
+import { postFindAll } from 'fake-restapi-node'
+
+console.log(postFindAll())
+```
--- index.js
+++ index.js
@@ -1,3 +1,47 @@
-const { postFindAll, postFindById, postCreate, postDelete, postUpdate } = require('./lib/services/posts')
+const { findAll, create, getById, deleteById, updateById} = require('./lib/services/posts')
 
-module.exports = { postFindAll, postFindById, postCreate, postDelete, postUpdate }
+async function findAllPosts() {
+  try {
+    const { data } = await findAll()
+    return data 
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+async function getPost(id) {
+  try {
+    const { data } = getById(id) 
+    return data
+  } catch (error) {
+    console.log(error) 
+  }
+}
+
+async function createPost(data) {
+  try {
+    const { data } = create(data) 
+    return data
+  } catch (error) {
+    console.log(error) 
+  }
+}
+
+async function deletePost(id) {
+  try {
+    const { data } = deleteById(id)
+    return data
+  } catch (error) {
+    console.log(error) 
+  }
+}
+
+async function updatePost(id, data) {
+  try {
+    const { data } = updateById(id, data) 
+    return data
+  } catch (error) {
+    console.log(error) 
+  }
+}
+module.exports = { findAllPosts, createPost, getPost, deletePost, updatePost}
